3a3f57a2bcd909703e2ea5a4e3e024a3d43f98fd
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2025-12-17 22:11:05 +0800

    Update ubuntu-trust.sh

--- ubuntu-trust.sh
+++ ubuntu-trust.sh
@@ -1,43 +1,20 @@
 #!/bin/bash
+set -euo pipefail
 
-source $INIT_CWD/.env
+source "${INIT_CWD}/.env"
 
-sudo cp $INIT_CWD/certs/ca.crt /usr/local/share/ca-certificates/$CN.crt && sudo update-ca-certificates
+if [[ -z "${CN:-}" ]]; then
+    echo "ERROR: CN is not set in .env"
+    exit 1
+fi
 
-### Script installs root.cert.pem to certificate trust store of applications using NSS
-### (e.g. Firefox, Thunderbird, Chromium)
-### Mozilla uses cert8, Chromium and Chrome use cert9
+certfile="${INIT_CWD}/certs/ca.crt"
+certname="${CN} Root CA"
 
-###
-### Requirement: apt install libnss3-tools
-###
+echo "Installing system-wide certificate..."
+sudo cp "${certfile}" /usr/local/share/ca-certificates/"${CN}.crt"
+sudo update-ca-certificates
 
-
-###
-### CA file to install (CUSTOMIZE!)
-###
-
-certfile="$INIT_CWD/certs/ca.crt"
-certname= echo "$CN Root CA"
-
-
-###
-### For cert8 (legacy - DBM)
-###
-
-for certDB in $(find ~/ -name "cert8.db")
-do
-    certdir=$(dirname ${certDB});
-    certutil -A -n "${certname}" -t "TCu,Cu,Tu" -i ${certfile} -d dbm:${certdir}
-done
-
-
-###
-### For cert9 (SQL)
-###
-
-for certDB in $(find ~/ -name "cert9.db")
-do
-    certdir=$(dirname ${certDB});
-    certutil -A -n "${certname}" -t "TCu,Cu,Tu" -i ${certfile} -d sql:${certdir}
-done
+echo "Installing user NSS DB certificate (~/.pki/nssdb)..."
+mkdir -p "${HOME}/.pki/nssdb"
+certutil -A -n "${certname}" -t "TCu,Cu,Tu" -i "${certfile}" -d sql:"${HOME}/.pki/nssdb"
